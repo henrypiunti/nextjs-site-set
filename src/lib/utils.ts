d7a4c72017fff77f75711b778b80e0d09227f627
Henry Piunti <henrylacavapiunti@gmail.com>
2026-08-08 15:12:19 -0300

    refactor(ui): simplifica variantes do Button e integra tokens de fonte
    Reduz Button a variantes primary/secondary e tamanhos default/lg, e
    estende o tailwind-merge para reconhecer as classes de tipografia
    customizadas (heading/body/action) definidas nos design tokens.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        {
+          text: [
+            'heading-hg',
+            'heading-xl',
+            'heading-lg',
+            'heading-md',
+            'heading-sm',
+            'heading-xs',
+            'body-md',
+            'body-sm',
+            'body-xs',
+            'action-md',
+            'action-sm',
+          ],
+        },
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
