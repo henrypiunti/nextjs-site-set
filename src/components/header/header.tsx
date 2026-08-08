e18e25740e326c6c30db998a26e5656c59515511
Henry Piunti <henrylacavapiunti@gmail.com>
2026-08-08 15:12:39 -0300

    style: ajusta responsividade do Header e Footer no mobile
    Adapta paddings e layout do nav do Footer para telas menores.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import { ActiveLink } from '../active-link';
 
 export function Header() {
   return (
-    <header className="flex h-24 justify-between border-b-[1.5px] border-gray-500 px-50">
+    <header className="flex h-24 justify-between border-b-[1.5px] border-gray-500 px-6 lg:px-50">
       <Image src="brand_logo.svg" alt="Logo" width={116} height={32} />
       <nav className="flex items-center gap-6">
         <ActiveLink href="/">Home</ActiveLink>
